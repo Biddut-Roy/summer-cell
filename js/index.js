f5d484418a80ed371dca1b1a7ecc2a4f35d90fbe
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-08-20 12:01:19 +0600

    compleat a main section

--- js/index.js
+++ js/index.js
@@ -19,15 +19,53 @@ function titleAdd(name){
 }
 
 function pricesAdd(price){
-    const primaryBtn = document.getElementById('pry-BTN');
+    const primaryBtn = document.getElementById('myButton');
     const totalPrices = document.getElementById('total-prices');
       total += parseFloat(price);
     const fixTotal = total.toFixed(2);
      totalPrices.innerText = fixTotal;
-     if(parseFloat(fixTotal) >= 200){
+     const primaryBtn1 = document.getElementById('myButton1');
+    const mainTotals = document.getElementById('total');
+    mainTotals.innerText = total;
+     if( total >= 200){
         primaryBtn.removeAttribute('disabled');
+        primaryBtn.style.backgroundColor = 'green';
+        // primaryBtn1.removeAttribute('disabled');
+        // primaryBtn1.style.backgroundColor = 'green';
+        
      }else{
-        primaryBtn.setAttributeAttribute('disabled', true);
+        primaryBtn.setAttribute('disabled', true);
+        // primaryBtn1.setAttribute('disabled', true);
      }
+    
 
+     purchase(primaryBtn1)
 }
+
+function couponCode(){
+    const coupon = document.getElementById('coupon');
+    const discount = document.getElementById('total-discount');
+    const mainTotal = document.getElementById('total');
+    const couponValue = coupon.value;
+    if( couponValue === "SELL200"){
+        const totalDiscount = (total * 20) / 100;
+        discount.innerText = totalDiscount;
+        mainTotal.innerText = total - totalDiscount;
+    }else{
+        mainTotal.innerText = total;
+    }
+}
+
+function purchase(primaryBtn1){
+
+
+    if( total >= 200){
+        primaryBtn1.removeAttribute('disabled');
+        primaryBtn1.style.backgroundColor = 'green';
+        
+     }else{
+        primaryBtn1.setAttribute('disabled', true);
+     }
+}
+
+
